1e91dc19f873c3fd8e07f08ed8c82aee9357cd74
Phong <epixdiamond2007@gmail.com>
2026-09-05 12:30:36 +0700

    Stack-1

--- stack.c
+++ stack.c
@@ -1,9 +1,32 @@
 #include <stdio.h>
+#include <string.h>
+#define MAX 100
+// Stack
+char stack[MAX];
+int top = -1;
 
-void reversestirng(s) {
-    
+void push(char c) {
+    stack[++top] = c;
+}
+char pop() {
+    return stack[top--];
+}
+void ReverseString(char *str) {
+    int len = strlen(str);
+
+    // Push vào đĩa
+    for (int i = 0; i < len; i++) {
+        push(str[i]);
+    }
+    // Pop ra
+    for (int i = 0; i < len; i++) {
+        str[i] = pop();
+    }
 }
 int main() {
-    printf("Hello world!");
+    char s[MAX];
+    fgets(s, sizeof(s), stdin);
+    ReverseString(s);
+    printf("%s", s);
     return 0;
 }
